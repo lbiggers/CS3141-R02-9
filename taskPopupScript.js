d6901e692776e1a34509b67dac89402ffe0a71ef
lbiggers <lbiggers@mtu.edu>
2022-10-25 17:08:25 -0400

    trying to get the form to work

--- taskPopupScript.js
+++ taskPopupScript.js
@@ -1,3 +1,40 @@
-function main(){
-	window.alert("test");
+function submit(){
+
+	let data = [4];
+/*
+0 - name
+1 - info
+2 - time array
+3 - freq array
+*/
+
+	data[0] = document.getElementById('nameInput').value;
+
+	data[1] = document.getElementById('infoInput').value;
+
+	data[2] = [
+		document.getElementById('timeInputHour').value,
+		document.getElementById('timeInputMinute').value,
+		document.getElementById('timeInputAmPm').value
+	];
+
+	data[3] = [
+		document.getElementById('freqSn').checked,
+		document.getElementById('freqMn').checked,
+		document.getElementById('freqTs').checked,
+		document.getElementById('freqWd').checked,
+		document.getElementById('freqTr').checked,
+		document.getElementById('freqFr').checked,
+		document.getElementById('freqSt').checked
+	];
+
+	if (window.opener != null && !window.opener.closed) {
+		var txtName = window.opener.document.getElementById("storeData");
+		txtName.value = data;
+	}
+	window.close();
+}
+
+function testFunc(){
+	window.opener.alert("test");
 }
