29dcf7e939437111a3f00d2d465e2501d086c713
lbiggers <lbiggers@mtu.edu>
2022-10-26 12:11:58 -0400

    Update task.js

--- task.js
+++ task.js
@@ -1,84 +1,30 @@
 class task
 {
-    constructor(name, info, type, frequency, lastFinished)
+    constructor(name, info, time, frequency)
     {
         this.name = name;                       //Display name of task
         this.info = info; 
-        this.type = type;                   //'D' or 'I'. Days or intervals.
+        this.time = time;                   //'D' or 'I'. Days or intervals.
         this.frequency = frequency;         //D could be any substring of "SMTWRFZ" (R=Thurs. Z=Sat). I is any integer
-        this.lastFinished = lastFinished;
-    }
-    getName()
-    {
-        return this.name;
     }
     getID()
     {
         return this.id;
     }
+    getName()
+    {
+        return this.name;
+    }
     getInfo()
     {
         return this.info;
     }
-    getType()
+    getTime()
     {
-        return this.type;
+        return this.time;
     }
     getFrequency()
     {
         return this.frequency;
-    }
-    getLastFinished()
-    {
-        return this.lastFinished;
-    }
-    doSunday()
-    {
-        if (this.type == 'D')
-        {
-            return this.frequency.includes("S");
-        }
-    }
-    doMonday()
-    {
-        if (this.type == 'D')
-        {
-            return this.frequency.includes("M");
-        }
-    }
-    doTuesday()
-    {
-        if (this.type == 'D')
-        {
-            return this.frequency.includes('T');
-        }
-    }
-    doWednesday()
-    {
-        if (this.type == 'D')
-        {
-            return this.frequency.includes("W");
-        }
-    }
-    doThursday()
-    {
-        if (this.type == 'D')
-        {
-            return this.frequency.includes("R");
-        }
-    }
-    doFriday()
-    {
-        if (this.type == 'D')
-        {
-            return this.frequency.includes("F");
-        }
-    }
-    doSaturday()
-    {
-        if (this.type == 'D')
-        {
-            return this.frequency.includes("Z");
-        }
     }
 }
