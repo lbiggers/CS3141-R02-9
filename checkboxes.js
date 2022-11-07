5b61771d8cfa6a0da9520eba2acd042cf36d7047
lbiggers <lbiggers@mtu.edu>
2022-11-07 12:24:58 -0500

    fdssdf

--- checkboxes.js
+++ checkboxes.js
@@ -117,6 +117,68 @@ function newTask(){
 				<option value='1'>PM</option>
 			</select>
 		</div>
+		<div class='item' id='date'>
+			<label id='dateLabel'>on what date: </label> 
+			<select id='dateInputMonth'>
+				<option value='1'>1</option>
+				<option value='2'>2</option>
+				<option value='3'>3</option>
+				<option value='4'>4</option>
+				<option value='5'>5</option>
+				<option value='6'>6</option>
+				<option value='7'>7</option>
+				<option value='8'>8</option>
+				<option value='9'>9</option>
+				<option value='10'>10</option>
+				<option value='11'>11</option>
+				<option value='12'>12</option>
+			</select>
+			<label>/</label>
+			<select id='dateInputDay'>
+				<option value='1'>1</option>
+				<option value='2'>2</option>
+				<option value='3'>3</option>
+				<option value='4'>4</option>
+				<option value='5'>5</option>
+				<option value='6'>6</option>
+				<option value='7'>7</option>
+				<option value='8'>8</option>
+				<option value='9'>9</option>
+				<option value='10'>10</option>
+				<option value='11'>11</option>
+				<option value='12'>12</option>
+				<option value='13'>13</option>
+				<option value='14'>14</option>
+				<option value='15'>15</option>
+				<option value='16'>16</option>
+				<option value='17'>17</option>
+				<option value='18'>18</option>
+				<option value='19'>19</option>
+				<option value='20'>20</option>
+				<option value='21'>21</option>
+				<option value='22'>22</option>
+				<option value='23'>23</option>
+				<option value='24'>24</option>
+				<option value='25'>25</option>
+				<option value='26'>26</option>
+				<option value='27'>27</option>
+				<option value='28'>28</option>
+				<option value='29'>29</option>
+				<option value='30'>30</option>
+				<option value='31'>31</option>
+			</select>
+			<label>/</label>
+			<select id='dateInputYear'>
+				<option value='2022'>2022</option>
+				<option value='2023'>2023</option>
+				<option value='2024'>2024</option>
+				<option value='2025'>2025</option>
+				<option value='2026'>2026</option>
+				<option value='2027'>2027</option>
+				<option value='2028'>2028</option>
+				<option value='2029'>2029</option>
+			</select>
+		</div>
 		<div class='item' id='freq'>
 			<label id='freqLabel'>which days:</label> 
 			<br>
@@ -204,6 +266,12 @@ function formSubmit(){
 		innerDoc.getElementById('freqTr').checked,
 		innerDoc.getElementById('freqFr').checked,
 		innerDoc.getElementById('freqSt').checked
+	];
+
+	data[4] = [
+		innerDoc.getElementById('dateInputMonth').value,
+		innerDoc.getElementById('dateInputDay').value,
+		innerDoc.getElementById('dateInputYear').value
 	];
 
 
