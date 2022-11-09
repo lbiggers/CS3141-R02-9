0d52b41fe52f62d4b5ba3b386c6103a5d3a1dbbf
jpbramer <114097875+jpbramer@users.noreply.github.com>
2022-11-09 11:26:24 -0500

    Added default and turquoise themes
    Added buttons and functionality for default and turquoise theme.

--- pretty.js
+++ pretty.js
@@ -6,3 +6,44 @@ function outlineTaskButton(){
 		button.setAttribute('style','border: none;');
 	});
 }
+function outlineMenuButton(){
+	let button = document.getElementById('menuButton');
+	button.setAttribute('style','border: 2px solid #00A000;');
+	button.setAttribute('style','border: 6px solid white;');
+	addEventListener('mouseout', (event) => {
+		button.setAttribute('style','border: none;');
+	});
+}
+
+function dropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+function changeThemeDefault(){
+	let button = document.getElementById('themeButton');
+	
+        document.documentElement.setAttribute('data-theme', 'root');
+     
+    toggleSwitch.addEventListener('change', switchTheme, false);
+}
+function changeThemeTurquoise(){
+	let button = document.getElementById('themeButton');
+	
+        document.documentElement.setAttribute('data-theme', 'turquoise');
+     
+    toggleSwitch.addEventListener('change', switchTheme, false);
+}
